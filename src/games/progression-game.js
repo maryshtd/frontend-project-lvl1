b9260f178e6d42a4f89453eff1aece7630127456
Mariia Shtoda <maryshtd@gmail.com>
2022-01-16 19:43:15 +0100

    Fixes after review #4

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -2,23 +2,22 @@ import getRandomNumber from '../getRandomNumber.js';
 
 export const progressionLength = 10;
 
-const generateProgression = (beginning, increase) => {
+const generateProgression = (beginning, increase, missingIndex) => {
   const progression = [];
   for (let i = 0; i <= progressionLength; i += 1) {
-    const newNumber = beginning + (increase * i);
-    progression.push(newNumber);
+    progression.push(beginning + (increase * i));
   }
-  const randomIndex = getRandomNumber(0, progressionLength - 1);
-  const expectedAnswer = progression[randomIndex];
-  progression[randomIndex] = '..';
+  progression[missingIndex] = '..';
   const progressionString = progression.join(' ');
-  return [progressionString, expectedAnswer];
+  return progressionString;
 };
 
 const progressionGame = () => {
   const randomBeginning = getRandomNumber(1, 10);
   const randomIncrease = getRandomNumber(1, 10);
-  const [progression, expectedAnswer] = generateProgression(randomBeginning, randomIncrease);
+  const randomIndex = getRandomNumber(0, progressionLength - 1);
+  const progression = generateProgression(randomBeginning, randomIncrease, randomIndex);
+  const expectedAnswer = randomBeginning + (randomIncrease * randomIndex);
   return [progression, String(expectedAnswer)];
 };
 
